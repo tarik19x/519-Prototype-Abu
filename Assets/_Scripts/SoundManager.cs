e835cbf4d5a11153fa406bb660ea3cafacb0a60b
Tarik Hasan <61243717+tarik19x@users.noreply.github.com>
2024-04-15 18:28:46 -0700

    Sound working

---
+++ Assets/_Scripts/SoundManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class SoundManager : SingletonMonoBehavior<SoundManager>
+{
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioSource effectsSource;
+
+    public void ChangeMasterVolume(float val)
+    {
+        AudioListener.volume = val;
+    }
+    public void PlaySound(AudioClip clip)
+    {
+        if (effectsSource.isPlaying)
+        {
+            effectsSource.Stop();
+        }
+        effectsSource.PlayOneShot(clip);
+    }
+    
+    public void ToggleMusic()
+    {
+        musicSource.mute = !musicSource.mute;
+    }
+    
+    public void ToggleEffects()
+    {
+        effectsSource.mute = !effectsSource.mute;
+    }
+}
